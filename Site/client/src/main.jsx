bd8e5dd0aefb7cbe8c66b6b8c1817bbce2474039
sadeheden <sadeheden@gmail.com>
2025-08-05 16:35:08 +0300

    .

--- Site/client/src/main.jsx
+++ Site/client/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './assets/styles/App.css'; // ✅ Import global styles
+import './assets/styles/support.css';
+
 
 
 createRoot(document.getElementById('root')).render(
