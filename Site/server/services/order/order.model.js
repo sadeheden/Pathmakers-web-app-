3949c0a1ac3b430ba37a9e3bf3aa9f5d16753c98
mayhalaf <155309787+mayhalaf@users.noreply.github.com>
2025-08-05 12:01:41 +0300

    pdf

--- Site/server/services/order/order.model.js
+++ Site/server/services/order/order.model.js
@@ -18,7 +18,7 @@ function toObjectIdOrString(id) {
 export default class Order {
   constructor(data = {}) {
     // For user_id, we need ObjectId for database operations
-    this.user_id = isValidObjectId(data.user_id) ?  ObjectId(data.user_id) : null;
+    this.user_id = isValidObjectId(data.user_id) ? new ObjectId(data.user_id) : null;
     
     // All IDs should be ObjectIds based on your database format
     this.departure_city_id = toObjectIdOrString(data.departure_city_id);
